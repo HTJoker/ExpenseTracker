8eb895cf7ea061ddc1e4191a0b8eb8e301964c7d
Hersh Thakur <hersh.thakur@gmail.com>
2024-03-05 12:21:26 +0000

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Hersh Thakur on 05/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
